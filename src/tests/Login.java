dbbeed8b7a320038ff47387fe74ec7df47c90db3
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 12:32:06 +0100

    update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,5 +3,7 @@ package tests;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Here er are. Let's login");
+
+        System.out.println("Hurray! I logged in.");
     }
 }

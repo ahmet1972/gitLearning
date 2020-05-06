5c72bfc175c3d0d14f7fdc5fa8e44215cdecc2f1
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 17:29:03 +0100

    add Winter.java

---
+++ src/tests/Winter.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Winter {
+    public static void main(String[] args) {
+
+        String trees= "yellow";
+        String river = "streaming wild";
+        String weather = "gloomy and rainy";
+        double wind = 20;
+
+    }
+}

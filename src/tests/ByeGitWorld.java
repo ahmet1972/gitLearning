05ec6d4450e8bfeb37e178b524bbb70902f64404
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 16:19:37 +0100

    add Spring.java

--- src/tests/ByeGitWorld.java
+++ src/tests/ByeGitWorld.java
@@ -3,5 +3,6 @@ package tests;
 public class ByeGitWorld {
     public static void main(String[] args) {
         System.out.println("Bye git! See you next time");
+        System.out.println("Let's compare the versions");
     }
 }
---
+++ src/tests/Spring.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Spring {
+    public static void main(String[] args) {
+
+    String trees;
+    String river;
+    String weather;
+    double wind;
+
+
+
+
+    }
+}

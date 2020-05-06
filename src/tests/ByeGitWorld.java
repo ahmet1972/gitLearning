b97d1519c2a464bd6ae21f133f44df8b35272e1d
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 12:26:48 +0100

    add Login.java

--- src/tests/ByeGitWorld.java
+++ src/tests/ByeGitWorld.java
@@ -1,4 +1,7 @@
 package tests;
 
 public class ByeGitWorld {
+    public static void main(String[] args) {
+        System.out.println("Bye git! See you next time");
+    }
 }
--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -5,4 +5,6 @@ public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Helo new world again");
     }
+
+
 }

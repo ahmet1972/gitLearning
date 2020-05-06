4d809818f10e9ba438dc9df177a5ef6b634b7d70
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 16:23:29 +0100

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -3,11 +3,12 @@ package tests;
 public class Spring {
     public static void main(String[] args) {
 
-    String trees;
-    String river;
-    String weather;
-    double wind;
+    String trees= "greeen";
+    String river = "streaming strong";
+    String weather = "shiny and full of life";
+    double wind = 10;
 
+        System.out.println("Let me know how Spring is like in your hometown?");
 
 
 

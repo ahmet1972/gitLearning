8c451dda4067327d93178a00e7d92c089a22031f
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-06 16:30:02 +0100

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -8,12 +8,14 @@ public class Spring {
     String weather = "shiny and full of life";
     double wind = 10;
 
+
         System.out.println("Let me know how Spring is like in your hometown?");
 
         System.out.println("It's wonderful like; trees are " + trees);
         System.out.println("It's wonderful like; rivers are " + river);
         System.out.println("It's wonderful like; weather is " + weather);
-        System.out.println("It's wonderful like; wind " + wind);
+        System.out.println("It's wonderful like; wind is max " + wind + "knot");
+
 
 
 

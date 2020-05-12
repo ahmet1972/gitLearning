0a0b8fe21ce4e5ef8bb73304e5bcb8eac17ce408
ahmet1972 <62038506+ahmet1972@users.noreply.github.com>
2020-05-12 16:06:39 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,6 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Practice makes it perfect");
+        System.out.println("I've made this change in Github")
     }
 }

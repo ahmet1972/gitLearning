96714bb2b23439f0eea2d97010a2db8a048704e1
ahmet1972 <62038506+ahmet1972@users.noreply.github.com>
2020-05-13 15:38:09 +0100

    Update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -5,5 +5,12 @@ public class Login {
         System.out.println("Here er are. Let's login");
 
         System.out.println("Hurray! I logged in.");
+        
+        
+        
+        
+        
+        
+        System.out.println("This is to study conflict on different lines to solve the conflict between lines 10 and 14"); 
     }
 }
